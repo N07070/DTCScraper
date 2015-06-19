603c55e364fa768a8f4da5070260de9d7265c669
N07070 <nelson.perdriau@gmail.com>
2015-06-19 21:14:24 +0200

    Added a way to display the quotes.

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ This script gets all the quotes from danstonchat, the french quote website.
 
 from bs4 import BeautifulSoup
 import urllib2
+import re
+
+url_dtc = "http://danstonchat.com/12.html"
+elements = []
+list_links = []
+liste_quote = []
+
 
 class DTCScrapper(object):
     """docstring for DTCScrapper"""
-    # def __init__(self, arg):
-    #     super(DTCScrapper, self).__init__()
-    #     self.arg = arg
 
     def get_web_page_content(self,url):
         web_page_content = ""
@@ -23,22 +27,23 @@ class DTCScrapper(object):
         print("I got the content from " + str(url))
         return web_page_content
 
-    def extract_quote_from_html(self,html_page):
-        soup = BeautifulSoup(html_page)
-        soup = soup.select('div .item-content span')
-        return soup
+    def extract_quote_from_html(self,html_page,url):
+        mushroomsoup = BeautifulSoup(html_page)
+        for every_content in mushroomsoup.find('a',{'href':url}):
+            every_content =  unicode(every_content).replace('<span class="decoration">',"")
+            every_content =  unicode(every_content).replace('</span>',"")
+            every_content =  unicode(every_content).replace('<br/>',"")
+            if every_content == '' or every_content == " ":
+                pass
+            else:
+                elements.append(every_content)
+
+        return elements
+
+    def main(self):
+        return self.extract_quote_from_html(self.get_web_page_content(url_dtc),url_dtc)
 
 e = DTCScrapper()
-derp = e.get_web_page_content("http://danstonchat.com/16561.html")
-print(derp)
-print(e.extract_quote_from_html(derp))
 
-[<p class="item-content">
-    <a href="http://danstonchat.com/16561.html">
-        <span class="decoration">A:</span> Tout a leur, j'ai laissé ma soeur utiliser le pc.<br/>
-        <span class="decoration">Z:</span> wow, quel courage!<br/>
-        <span class="decoration">A:</span> Oui, rigole...<br/>
-        <span class="decoration">A:</span> Je lui dit "pour scroller avec le touchpad, il faut utiliser les deux doitgs".<br/>
-        <span class="decoration">A:</span> Elle m'a regardé bizarre...
-    </a>
-</p>]
+for a in e.main():
+    print(unicode(a))
