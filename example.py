424f22fd107ff866f377678df82c5626f43706a3
N07070 <nelson.perdriau@gmail.com>
2015-07-19 22:41:22 +0200

    Changed the way you use the example.

--- example.py
+++ example.py
@@ -4,16 +4,18 @@
 from DTCScrapper import DTCScrapper
 import sys
 
+# Get the quote number from the arguments
 user_limit = sys.argv[1]
 
+# Initiate the class
 e = DTCScrapper()
+
+# Create the url
 url_dtc = "http://danstonchat.com/"+str(user_limit)+".html"
 
-print("Quote n°"+ str((url_dtc.replace("http://danstonchat.com/","").replace(".html",""))))
-print("==================")
-try:
-    for a in e.main(url_dtc):
-        print(unicode(a))
-    print("==================")
-except:
-    pass
+# Get the results
+result_from_scrapper =  e.main(url_dtc)
+
+# Print out each value
+for value in result_from_scrapper:
+    print(value)
